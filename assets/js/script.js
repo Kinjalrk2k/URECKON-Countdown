73308a329cc070a155f7af7d1a744fb8e055260e
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2020-08-26 13:31:28 +0530

    Added background video

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,20 @@
-const second = 1000,
-  minute = second * 60,
-  hour = minute * 60,
-  day = hour * 24;
+var tag = document.createElement('script');
 
-let countDown = new Date("Sep 30, 2020 00:00:00").getTime(),
-  x = setInterval(function () {
-    let now = new Date().getTime(),
-      distance = countDown - now;
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+var player;
 
-    (document.getElementById("days").innerText = Math.floor(distance / day)),
-      (document.getElementById("hours").innerText = Math.floor(
-        (distance % day) / hour
-      )),
-      (document.getElementById("minutes").innerText = Math.floor(
-        (distance % hour) / minute
-      )),
-      (document.getElementById("seconds").innerText = Math.floor(
-        (distance % minute) / second
-      ));
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    videoId: 'oxICyKA-TxI',
+    events: {
+      'onReady': onPlayerReady,
+    }
+  });
+}
 
-    //do something later when date is reached
-    //if (distance < 0) {
-    //  clearInterval(x);
-    //  'IT'S MY BIRTHDAY!;
-    //}
-  }, second);
-
-document.addEventListener("DOMContentLoaded", () => {
-  // Unix timestamp (in seconds) to count down to
-  var twoDaysFromNow = new Date().getTime() / 1000 + 86400 * 2 + 1;
-
-  // Set up FlipDown
-  var flipdown = new FlipDown(twoDaysFromNow)
-
-    // Start the countdown
-    .start()
-
-    // Do something when the countdown ends
-    .ifEnded(() => {
-      console.log("The countdown has ended!");
-    });
-
-  // Toggle theme
-  var interval = setInterval(() => {
-    let body = document.body;
-    body.classList.toggle("light-theme");
-    body.querySelector("#flipdown").classList.toggle("flipdown__theme-dark");
-    body.querySelector("#flipdown").classList.toggle("flipdown__theme-light");
-  }, 5000);
-
-  var ver = document.getElementById("ver");
-  ver.innerHTML = flipdown.version;
-});
+function onPlayerReady(event) {
+  event.target.mute();
+  event.target.playVideo();
+}
